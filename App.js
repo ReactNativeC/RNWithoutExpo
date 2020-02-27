6a99bcc9d6a79a5ac0923c7c8de3584b7be8ee21
Noor <noormahammad@users.noreply.github.com>
2020-02-27 13:00:52 -0500

    picking a single image in react native using react-native-image-picker

--- App.js
+++ App.js
@@ -1,29 +1,62 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Button, Alert, View, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, Button, Alert, View, Text, Image} from 'react-native';
 
-const App = () => {
+import ImagePicker from 'react-native-image-picker';
+
+const App = () => {  
+  const pickImageHandler = () => {
+    // More info on all the options is below in the API Reference... just some common use cases shown here
+    const options = {
+      title: 'Select Avatar',
+      customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+
+    /**
+     * The first arg is the options object for customization (it can also be null or omitted for default options),
+     * The second arg is the callback which sends object: response (more info in the API Reference)
+     */
+    ImagePicker.showImagePicker(options, (response) => {
+      //console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      } else {
+        const source = { uri: response.uri };
+        
+      console.log(source);   
+      }
+    });
+  }
+
+  const pickMultipleImageHandler = () => {
+
+  }
   return (    
       <SafeAreaView style={styles.screen}>
         <View style={styles.box}>
           <Button
             title="Pick an Image"
-            onPress={() => {
-              Alert.alert('Hello', 'You Pressed button', [{title: 'OK'}]);
-            }}
+            onPress={pickImageHandler}            
           />
           <Text style={styles.text}>using https://github.com/react-native-community/react-native-image-picker</Text>
         </View>
         <View style={styles.box}>
           <Button
             title="Pick Multiple Images"
-            onPress={() => {
-              Alert.alert('Hello', 'You Pressed button', [{title: 'OK'}]);
-            }}
+            onPress={pickMultipleImageHandler}
           />
           <Text style={styles.text}>using https://github.com/ivpusic/react-native-image-crop-picker</Text>
         </View>
       </SafeAreaView>    
-  );
+  );  
 };
 
 const styles = StyleSheet.create({
