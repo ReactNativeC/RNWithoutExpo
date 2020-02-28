e4ba6d68838d2a083616b5c03201487e5b039941
Noor <noormahammad@users.noreply.github.com>
2020-02-28 07:06:24 -0500

    configuring android and ios settings and directly opening the camera or image library

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {SafeAreaView, StyleSheet, Button, Alert, View, Text, Image} from 'react-native';
 
 import ImagePicker from 'react-native-image-picker';
 
 const App = () => {  
+  const [pickedImageUri, setPickedImageUri] = useState();
   const pickImageHandler = () => {
     // More info on all the options is below in the API Reference... just some common use cases shown here
     const options = {
@@ -36,8 +37,50 @@ const App = () => {
     });
   }
 
-  const pickMultipleImageHandler = () => {
+  const launchCameraHandler = () => {
+    const options = {
+      title: 'Select Avatar',
+      customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    ImagePicker.launchCamera(options, (response) => {
+      if(response.didCancel) {
+        console.log('User cancelled');
+      } else if (response.error) {
+        console.log('ImagePicker error: ', response.error)
+      } else {
+        const imageUri = response.uri;
+        console.log(imageUri);
+        setPickedImageUri(imageUri);
+      }
+    })
+  }
+  const LaunchImageLibraryHandler = () => {
+    const options = {
+      title: 'Select Avatar',
+      customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    ImagePicker.launchImageLibrary(options, (response) => {
+      if(response.didCancel) {
+        console.log('User cancelled');
+      } else if (response.error) {
+        console.log('ImagePicker error: ', response.error)
+      } else {
+        const imageUri = response.uri;
+        console.log(imageUri);
+        setPickedImageUri(imageUri);
+      }
+    })
+  }
 
+  const pickMultipleImageHandler = () => {
   }
   return (    
       <SafeAreaView style={styles.screen}>
@@ -46,14 +89,27 @@ const App = () => {
             title="Pick an Image"
             onPress={pickImageHandler}            
           />
-          <Text style={styles.text}>using https://github.com/react-native-community/react-native-image-picker</Text>
+          <View style={{marginTop:10}}>
+          <Button
+            title="Launch Camera"
+            onPress={launchCameraHandler}            
+          />
+          </View>
+          <View style={{marginTop: 10}}>
+           <Button
+            title="Launch Image Library"
+            onPress={LaunchImageLibraryHandler}            
+          />
+          </View>
+          <Text style={styles.text}>Using https://github.com/react-native-community/react-native-image-picker</Text>
+          <Image source={{uri: pickedImageUri}}  style={{width: 100, height: 100}}/>
         </View>
         <View style={styles.box}>
           <Button
             title="Pick Multiple Images"
             onPress={pickMultipleImageHandler}
           />
-          <Text style={styles.text}>using https://github.com/ivpusic/react-native-image-crop-picker</Text>
+          <Text style={styles.text}>Using https://github.com/ivpusic/react-native-image-crop-picker</Text>
         </View>
       </SafeAreaView>    
   );  
@@ -73,11 +129,11 @@ const styles = StyleSheet.create({
     marginVertical: 10,          
   }, 
   text: {    
-    fontSize: 20, 
+    fontSize: 16, 
     color: 'gray',
     marginHorizontal: 10,
+    marginTop:5
   }
-
 })
 
 export default App;
